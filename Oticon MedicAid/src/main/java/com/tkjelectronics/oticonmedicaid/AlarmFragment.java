2894774cb2aac2ce783db088158fe45dafaa2fe7
Kristian Lauszus <lauszus@gmail.com>
2014-05-06 00:56:50 +0200

    Got notification partly working, but it keeps creating a new activity when clicked
    Also tried to restore the state, but that didn't work
    
    So it is just commented out for now

--- Oticon MedicAid/src/main/java/com/tkjelectronics/oticonmedicaid/AlarmFragment.java
+++ Oticon MedicAid/src/main/java/com/tkjelectronics/oticonmedicaid/AlarmFragment.java
@@ -1,7 +1,5 @@
 package com.tkjelectronics.oticonmedicaid;
 
-import android.content.Context;
-import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,8 +8,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-// TODO: Play alarm sound and then dismiss notification using button
-// TODO: Make notification
 public class AlarmFragment extends Fragment {
     Button dismissBtn;
     TextView mTextView;
@@ -29,6 +25,23 @@ public class AlarmFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_alarm, container, false);
         dismissBtn = (Button) view.findViewById(R.id.dismissButton);
+        mTextView = (TextView) view.findViewById(R.id.reminderText);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (alarmFlag) {
+            mTextView.setVisibility(View.VISIBLE);
+            dismissBtn.setText(getString(R.string.dismiss));
+            /*AudioManager audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+            audioManager.requestAudioFocus((MedicAidActivity) getActivity(), AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);*/
+            ((MedicAidActivity) getActivity()).startMediaPlayer();
+        } else
+            mTextView.setVisibility(View.GONE);
+
         dismissBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,26 +51,5 @@ public class AlarmFragment extends Fragment {
                 ((MedicAidActivity) getActivity()).stopMediaPlayer();
             }
         });
-        mTextView = (TextView) view.findViewById(R.id.reminderText);
-
-        if (alarmFlag)
-            setAlarm();
-        else
-            mTextView.setVisibility(View.GONE);
-
-        return view;
-    }
-
-    private void setAlarm() {
-        mTextView.setVisibility(View.VISIBLE);
-        dismissBtn.setText(getString(R.string.dismiss));
-
-        AudioManager audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
-        int result = audioManager.requestAudioFocus(((MedicAidActivity) getActivity()), AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-/*
-        if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-            // Could not get audio focus.
-        }
-*/
     }
 }
--- Oticon MedicAid/src/main/java/com/tkjelectronics/oticonmedicaid/MedicAidActivity.java
+++ Oticon MedicAid/src/main/java/com/tkjelectronics/oticonmedicaid/MedicAidActivity.java
@@ -1,7 +1,6 @@
 package com.tkjelectronics.oticonmedicaid;
 
 import android.app.AlertDialog;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,12 +18,16 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+// TODO: Dismiss notification using button and make notification
 public class MedicAidActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks, AudioManager.OnAudioFocusChangeListener {
     private static final String TAG = "MedicAidActivity";
     public static final boolean D = BuildConfig.DEBUG; // This is automatically set when building
 
     public static final String EXTRA_ALARM = "ALARM";
+    //public static final String EXTRA_PLAYER_POS = "PLAYER_POS";
+
+    public static final int REMINDER_FRAGMENT = 0;
+    public static final int CALENDAR_FRAGMENT = 1;
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
@@ -39,6 +42,8 @@ public class MedicAidActivity extends ActionBarActivity implements NavigationDra
     private CalendarReminderReceiver mCalendarReminderReceiver = new CalendarReminderReceiver();
 
     private MediaPlayer mMediaPlayer;
+    //private int playerPosition = 0; // Zero by default
+    //private static final int notificationID = 0;
 
     private AlarmFragment mAlarmFragment;
     private CalendarFragment mCalendarFragment;
@@ -60,6 +65,42 @@ public class MedicAidActivity extends ActionBarActivity implements NavigationDra
         registerReceiver(mCalendarReminderReceiver, filter);
     }
 
+    public void startMediaPlayer() {
+        //playerPosition = pos;
+        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+    }
+
+/*
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (D)
+            Log.d(TAG, "---- onRestoreInstanceState ----");
+
+        int pos = savedInstanceState.getInt(EXTRA_PLAYER_POS, -1);
+        if (pos != -1) {
+            if (D)
+                Log.i(TAG, "Restoring MediaPlayer");
+            startMediaPlayer(pos); // Restore last player position
+            restoreAlarmFragment(); // Restore the fragment as well
+        } else if (D)
+            Log.i(TAG, "DO NOT Restoring MediaPlayer");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (D)
+            Log.d(TAG, "---- onSaveInstanceState ----");
+
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying())
+            playerPosition = mMediaPlayer.getCurrentPosition();
+        else
+            playerPosition = -1;
+        outState.putInt(EXTRA_PLAYER_POS, playerPosition);
+    }
+*/
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
@@ -69,10 +110,13 @@ public class MedicAidActivity extends ActionBarActivity implements NavigationDra
 
         if (D)
             Log.i(TAG, "Alarm flag: " + alarmFlag);
-        if (alarmFlag) {
-            replaceFragment(0, true);
-            mNavigationDrawerFragment.setFocus(0);
-        }
+        if (alarmFlag)
+            restoreAlarmFragment();
+    }
+
+    private void restoreAlarmFragment() {
+        replaceFragment(REMINDER_FRAGMENT, true);
+        mNavigationDrawerFragment.setFocus(REMINDER_FRAGMENT);
     }
 
     @Override
@@ -102,20 +146,19 @@ public class MedicAidActivity extends ActionBarActivity implements NavigationDra
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // Update the main content by replacing fragments
-        replaceFragment(position, false);
+        replaceFragment(position, false); // Update the main content by replacing fragments
     }
 
     private void replaceFragment(int position, boolean alarmFlag) {
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if (position == 0) {
+        if (position == REMINDER_FRAGMENT) {
             mTitle = getString(R.string.reminders_title);
             if (mAlarmFragment == null || alarmFlag)
                 mAlarmFragment = new AlarmFragment(alarmFlag);
             fragmentManager.beginTransaction()
                     .replace(R.id.container, mAlarmFragment, mTitle.toString())
                     .commit();
-        } else if (position == 1) {
+        } else if (position == CALENDAR_FRAGMENT) {
             mTitle = getString(R.string.calendar_title);
             if (mCalendarFragment == null)
                 mCalendarFragment = new CalendarFragment();
@@ -159,21 +202,60 @@ public class MedicAidActivity extends ActionBarActivity implements NavigationDra
 
     public void onAudioFocusChange(int focusChange) {
         if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+            //showNotification(notificationID);
+
             mMediaPlayer = MediaPlayer.create(this, R.raw.sounds_882_solemn);
             mMediaPlayer.setVolume(1.0f, 1.0f);
             mMediaPlayer.setLooping(true);
+            /*if (playerPosition > 0)
+                mMediaPlayer.seekTo(playerPosition);*/
             mMediaPlayer.start(); // No need to call prepare(); create() does that for you
             mMediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
         } else
             stopMediaPlayer();
     }
+/*
+    private void showNotification(int id) {
+        // The stack builder object will contain an artificial back stack for the started Activity.
+        // This ensures that navigating backward from the Activity leads out of your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this)
+                .addParentStack(MedicAidActivity.class) // Adds the back stack for the Intent (but not the Intent itself)
+                .addNextIntent(new Intent(this, MedicAidActivity.class)); // Adds the Intent that starts the Activity to the top of the stack
 
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentIntent(resultPendingIntent)
+                .setContentTitle(getString(R.string.takePill))
+                //.setContentText("Press here to dismiss")
+                .setSmallIcon(android.R.drawable.ic_dialog_alert);
+
+        Notification notification;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            notification = builder.build(); // This is only available from API level 16
+        else
+            notification = builder.getNotification();
+
+        notification.flags |= Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR | Notification.FLAG_SHOW_LIGHTS;
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(id, notification); // Show notification
+    }
+
+    private void hideNotification(int id) {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(id); // Hide notification
+    }
+*/
     public void stopMediaPlayer() {
         if (mMediaPlayer != null) {
             if (mMediaPlayer.isPlaying())
                 mMediaPlayer.stop();
+            mMediaPlayer.reset();
             mMediaPlayer.release();
             mMediaPlayer = null;
+
+            //hideNotification(notificationID);
         }
     }
 }
